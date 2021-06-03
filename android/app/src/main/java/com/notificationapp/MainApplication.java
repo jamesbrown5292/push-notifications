91d374b258544d141ff7599a35975847f03f9048
James Brown <jamesbrown5292@gmail.com>
2021-06-02 17:21:23 -0700

    symbol errors

--- android/app/src/main/java/com/notificationapp/MainApplication.java
+++ android/app/src/main/java/com/notificationapp/MainApplication.java
@@ -2,6 +2,7 @@ package com.notificationapp;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -10,6 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;  // <--- Import Package
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -44,7 +47,25 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+
+  private void createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
   }
 
   /**
